6b7df38108cbaa07cf11cdd62c0f91380e1cb614
Simon Aquino <siaquino@ebay.com>
2020-05-04 16:20:20 +0200

    Update CV

--- infra/public_bucket.tf
+++ infra/public_bucket.tf
@@ -7,16 +7,30 @@ resource "google_storage_bucket" "simon_public" {
   }
 }
 
-resource "google_storage_bucket_object" "cv" {
+resource "google_storage_bucket_object" "cv_pdf" {
   name   = "simon-aquino-cv.pdf"
   source = "../assets/SimonAquinoCV.pdf"
   bucket = google_storage_bucket.simon_public.name
 }
 
-resource "google_storage_object_access_control" "cv" {
-  object = google_storage_bucket_object.cv.output_name
+resource "google_storage_bucket_object" "cv_txt" {
+  name   = "simon-aquino-cv.txt"
+  source = "../assets/SimonAquinoCV.txt"
+  bucket = google_storage_bucket.simon_public.name
+}
+
+resource "google_storage_object_access_control" "cv_pdf" {
+  object = google_storage_bucket_object.cv_pdf.output_name
   bucket = google_storage_bucket.simon_public.name
   role   = "READER"
   entity = "allUsers"
 }
+
+resource "google_storage_object_access_control" "cv_txt" {
+  object = google_storage_bucket_object.cv_txt.output_name
+  bucket = google_storage_bucket.simon_public.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
 
